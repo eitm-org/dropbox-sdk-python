c65f06ceb62698d0977ed8c4a5669a946a7b09fd
xingyaoc <93013857+xingyaoc@users.noreply.github.com>
2023-02-13 14:29:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ version = eval(line.split('=', 1)[1].strip())  # pylint: disable=eval-used
 install_reqs = [
     'requests >= 2.16.2',
     'six >= 1.12.0',
-    'stone >= 2.*',
+    'stone >= 3.3.1',
 ]
 
 setup_requires = [
